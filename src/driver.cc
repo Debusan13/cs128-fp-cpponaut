b51a5e2cebc51159c5ca8c52c08d09d0e27edb91
Mark <bf-109g2@hotmail.com>
2021-12-07 16:08:12 -0600

    Remove Redundant Code in Driver

--- src/driver.cc
+++ src/driver.cc
@@ -11,12 +11,5 @@ using namespace ox;
 int main() {
   auto loader = DataLoader("example_data/example_1.csv");
   FileSystem::Init(loader);
-  auto textbooks_dir = FileSystem::GetInstance().GetNode("/Users/devak/Documents");
-
-  for (const auto& child : textbooks_dir->children) {
-    std::cout << (child->node_type == NodeType::File ? "File: " : "Dir: ")
-              << child->name << " size of " << child->inherit_size << std::endl;
-  }
-
   return ox::System{}.run<App>();
 }
